818b4d1f13be3c833ca81f67752208d25c3ecb26
Jake Brown <jakebrown188@gmail.com>
2020-02-27 21:34:06 -0500

    Update grr-dev-install.sh

--- grr-dev-install.sh
+++ grr-dev-install.sh
@@ -1,13 +1,22 @@
+# Step 1
 git clone https://github.com/google/grr
 cd grr/
+
+# Step 2
 sudo apt-get install virtualenv -y
 virtualenv ~/.virtualenv/GRR --python=python3
 source ~/.virtualenv/GRR/bin/activate
 pip install --upgrade pip wheel setuptools six
+
+# Step 3
 pip install nodeenv
 nodeenv -p --prebuilt
 source ~/.virtualenv/GRR/bin/activate
+
+# Step 4
 sudo apt-get install libssl-dev python-dev python-pip wget openjdk-8-jdk zip dh-systemd libmysqlclient-dev -y
+
+# Step 5
 pip install -e grr/proto
 pip install -e grr/core
 pip install -e grr/client
@@ -16,3 +25,12 @@ pip install -e grr/client_builder
 pip install -e grr/server/[mysqldatastore]
 pip install -e colab
 pip install -e grr/test
+
+# Step 7
+pip install --no-cache-dir -f https://storage.googleapis.com/releases.grr-response.com/index.html grr-response-templates
+
+# Step 8
+apt install -y mysql-server
+
+# Step 9
+grr_config_updater initialize
